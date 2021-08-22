42261740205b321c1197b609166f9ea18f4a4785
sudzhaev <sudzhaev.da@gmail.com>
2021-08-22 15:56:46 +0300

    session service fixed

--- src/main/kotlin/com/glothapp/api/config/auth/MockSessionService.kt
+++ src/main/kotlin/com/glothapp/api/config/auth/MockSessionService.kt
@@ -7,13 +7,13 @@ import javax.servlet.http.HttpServletRequest
 import javax.servlet.http.HttpServletResponse
 
 @Component
-class MockSessionService: SessionService<IdOfProfile> {
+class MockSessionService : SessionService<IdOfProfile> {
 
     override fun createSession(user: IdOfProfile, response: HttpServletResponse) {
         TODO("Not yet implemented")
     }
 
     override fun resolveUser(request: HttpServletRequest): IdOfProfile? {
-        return IdOfProfile(request.getHeader("X-SESSION").toInt())
+        return IdOfProfile(request.getHeader("X-SESSION")?.toInt() ?: return null)
     }
 }
